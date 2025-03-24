7f81a6f0381edff8a03da763d42e5cdaa717e499
Vhmed <vhmdhoss@gmail.com>
2025-03-24 20:26:47 +0200

    Add files via upload

---
+++ src/helpers/devspace.ts
@@ -0,0 +1,1 @@
+export default function DeveelopersSpace() {console.info("%c Stop Here! ","font-size: 20px; color: #DC143C; padding: 10px; border-radius: 10px; border: 1px solid #DC143C;"); console.info("%cThis is a browser feature intended for developers. Do not copy or paste anything here.","font-size: 14px; color: #DC143C;");}
